3630ee73c9be36aa1437e19c721d84a04092a486
kausiksubramaniyam <59435311+kausiksubramaniyam@users.noreply.github.com>
2021-11-27 18:23:25 +0530

    Update backgroundsubtractor.py

--- backgroundsubtractor.py
+++ backgroundsubtractor.py
@@ -1,17 +1,22 @@
 import cv2
 import numpy as np
-a=input("enter path:")
-if(a.isdigit()==True):
-	a=int(a)
-cap =cv2.VideoCapture(a)
-bgsubs = cv2.createBackgroundSubtractorMOG2()
-while True:
-	ret,frame =cap.read()
-	mask = bgsubs.apply(frame)
-	frame=cv2.bitwise_and(frame,frame,mask=mask)
-	cv2.imshow('Background Removed' , frame)
-	if cv2.waitKey(5) & 0xFF==ord('q'):
-	 	break;
 
-cap.release()
-cv2.destroyAllWindows()
+def backgroundsubtract(a):
+	cap =cv2.VideoCapture(a)
+	bgsubs = cv2.createBackgroundSubtractorMOG2()
+	while True:
+		ret,frame =cap.read()
+		mask = bgsubs.apply(frame)
+		frame=cv2.bitwise_and(frame,frame,mask=mask)
+		cv2.imshow('Background Removed' , frame)
+		if cv2.waitKey(5) & 0xFF==ord('q'):
+	 		break;
+
+	cap.release()
+	cv2.destroyAllWindows()
+
+if __name__=="__main__":
+	a=input("enter path:")
+	if(a.isdigit()==True):
+		a=int(a)
+	backgroundsubtract(a)
